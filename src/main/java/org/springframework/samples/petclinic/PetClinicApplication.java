1c9976dd0dee6af5f14fd70b173be1c11118a9d3
rabeykimo-max <rabeykimo@gmail.com>
2026-09-24 12:17:08 +0000

    Add PetClinic startup log

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -30,6 +30,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+		System.out.println("PetClinic DEV release started");
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
